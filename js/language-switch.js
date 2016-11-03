ffeb655ca69cdc7d85871b4db72900ef716d46e1
Abdullah Bashir <mabdullah353@gmail.com>
2016-11-03 16:20:02 +0500

    [BEAM-752] Fixed empty code nav and added ability to auto-detect languages

--- js/language-switch.js
+++ js/language-switch.js
@@ -1,24 +1,57 @@
 $(document).ready(function(){
-    // Adding Navigation on top of code blocks, to show supported languages.
-    var langSwitcherNav = "<div class=\"language-switcher col-sm-offset-10 col-sm-2\"> \
-        <ul class=\"nav nav-tabs nav-justified\"> \
-            <li class=\"active\" data-lang=\"java\"><a>Java</a></li> \
-            <li data-lang=\"python\"><a>Python</a></li> \
-        </ul> \
-    </div>";
-    $("div.language-java").before(langSwitcherNav);
+    // Pattern in every code block, wrapper element.
+    var elAttr = "div[class^=\"language-\"]";
+    // Default language.
+    var defLang = "language-java";
 
-    // Attaching on click listener, to li elements.
-    $(".language-switcher ul li").click(function(el){
-        // Making language preferences presistance, for user.
-        localStorage.setItem('langPref', $(this).data("lang"));
-        switchLanguage();
-    })
+    /**
+      * @desc Generate bootstrapped like nav template, showing supported languages in nav.
+      * @param array $languages - list of supported languages.
+      * @return string - html template, which is bootstrapped nav tabs.
+    */
+    function navTemplate(languages) {
+        var langList="";
 
-    // Performing language switch operation.
+        for (var i = languages.length - 1; i >= 0; i--) {
+            langList+="<li data-lang=\""+languages[i]+"\"> \
+                <a>"+languages[i].replace("language-","")+"</a></li>";
+        }
+        return "<div class=\"language-switcher "+languages.join(" ")+"\"> \
+                <ul class=\"nav nav-tabs\">"+langList+"</ul> </div>";
+    }
+
+    /**
+      * @desc Extract language from provided text.
+      * @param string $text - string containing language, e.g language-python.
+      * @return string - cleaned name of languge, e.g python.
+    */
+    function getLang(text) {
+        var lang = /language-\w+/i.exec(text)
+        return (lang)? lang[0]:'';
+    }
+
+    /**
+      * @desc Search next sibling and if it's also a code block, then store
+            it's language and move onto the next element. It will keep
+            looking untill their is no direct code block decendent left.
+      * @param object $el - jQuery object, from where to start searching.
+      * @param array $lang - list to hold languages, found while searching.
+      * @return array - list of languages found.
+    */
+    function nextFetch(el, lang) {
+        if(!el.is(elAttr)) {
+            return lang;
+        }
+
+        lang.push(getLang(el.attr('class')))
+        return nextFetch(el.next(), lang)
+    }
+
+    /**
+      * @desc Perform language switch on page, and persist it's state.
+    */
     var switchLanguage = function() {
-        var langPref = localStorage.getItem('langPref') || 'java';
-        var langToHide = (langPref === 'java')? 'python': 'java';
+        var langPref = localStorage.getItem('langPref') || defLang;
 
         // Adjusting active elements in navigation header.
         $(".language-switcher li").removeClass('active').each(function(){
@@ -28,9 +61,24 @@ $(document).ready(function(){
         });
 
         // Swapping visibility of code blocks.
-        $(".language-"+langToHide).hide();
-        $(".language-"+langPref).show();
+        $("[class^=\"language-\"]").hide();
+        $("."+langPref).show();
     }
+
+    // Add Nav tabs on top of code blocks.
+    $(elAttr).each(function() {
+        if($(this).prev().is(elAttr)) {
+            return;
+        }
+        $(this).before(navTemplate(nextFetch($(this), [])));
+    });
+
+    // Attaching on click listener, to li elements.
+    $(".language-switcher ul li").click(function(el) {
+        // Making language preferences presistance, for user.
+        localStorage.setItem('langPref', $(this).data("lang"));
+        switchLanguage();
+    })
 
     // Invoking on page boot.
     switchLanguage();
